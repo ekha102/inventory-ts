7123650aa7add1f96d2d142690046ca20e6bbe5f
Eric Kha <erkha102@gmail.com>
2024-08-21 23:24:26 -0700

    Add the button to create the store

--- src/sections/stores/create-store.tsx
+++ src/sections/stores/create-store.tsx
@@ -1,10 +1,50 @@
+import { AppBar, Button, Dialog, IconButton, List, ListItemButton, ListItemText, Toolbar, Typography } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close';
+
+
+interface IStores {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+export default function CreateStore( {open, setOpen}: IStores ) {
 
 
 
-export default function CreateStore() {
-  return(
+
+
+  return (
     <>
-      <h1>Create Store</h1>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={ ()=>setOpen(false) }
+      >
+        <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={()=>setOpen(false)}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Add Store
+            </Typography>
+
+            <Button autoFocus color="inherit" onClick={()=>setOpen(false)}>
+              save
+            </Button>
+
+          </Toolbar>
+        </AppBar>
+        <List>
+          {/* Form place here  */}
+        </List>
+      </Dialog>
+
     </>
   );
 }
--- src/sections/stores/view/stores.tsx
+++ src/sections/stores/view/stores.tsx
@@ -1,11 +1,20 @@
+import { Button } from "@mui/material";
 import CreateStore from "../create-store";
 import StoreTable from "../table-stores";
+import { useState } from "react";
+
 
 
 export default function Stores() {
+  
+  const [open, setOpen] = useState<boolean>(false);
+
+
   return (
     <>
-      <CreateStore/>
+      <Button variant="contained" color="primary" onClick={()=>setOpen(true)} >Create Store</Button>
+      {open ? <CreateStore open={open} setOpen={setOpen} /> : false}
+      
       <StoreTable/>
     </>
   );
